181c80643294cb0281e215415ab0e6b4e0836fc0
justmediego <tristannababan@student.unud.ac.id>
2021-11-30 23:26:38 +0700

    modul 5

---
+++ app/src/main/java/com/praktikum8/perpustakaan/ui/download/DownloadFragment.java
@@ -0,0 +1,35 @@
+package com.praktikum8.perpustakaan.ui.download;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.praktikum8.perpustakaan.R;
+
+public class DownloadFragment extends Fragment {
+
+    private DownloadViewModel downloadViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        downloadViewModel =
+                new ViewModelProvider(this).get(DownloadViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_download, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        downloadViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
