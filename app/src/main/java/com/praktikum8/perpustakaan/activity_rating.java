d89ce1fdb634a222ab1f839720292181d90a6bd9
jibrilaliakbar <jibrilaliakbarrr@gmail.com>
2021-12-08 14:27:12 +0700

    rating activity

---
+++ app/src/main/java/com/praktikum8/perpustakaan/activity_rating.java
@@ -0,0 +1,14 @@
+package com.praktikum8.perpustakaan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_rating extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+    }
+}
